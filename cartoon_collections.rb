d22a2180511a79ae3301b56bc90649ce563dce81
Akshay K <akshaykunti1@gmail.com>
2020-01-05 07:49:54 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,12 @@ def long_planeteer_calls(words)
   words.find {|word| word.length > 4} ? true : false
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  res = nil
+  cheese_types.each do |type|
+    array.include?(type)? res =type : res 
+    break if res
+  end
 end
