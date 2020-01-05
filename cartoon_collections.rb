7de4873aab1249eb6051079027f415eb33804b2b
Akshay K <akshaykunti1@gmail.com>
2020-01-05 07:39:22 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,7 +9,7 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  array.include?{|e| e.size > 4 }
+  array.include?{e.size > 4 }
 end
 
 def find_the_cheese# code an argument here
